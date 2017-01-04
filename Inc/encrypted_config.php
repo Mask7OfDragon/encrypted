2b5fd3f66332f4902a7fb2b1ee1676c4fee6a371
Mask7OfDragon <pipocastro99@hotmail.com>
2017-01-04 15:00:09 -0500

    Add Main Configuration File
    This file is in charge of controlling the data encryption and the restoration of the same

---
+++ Inc/encrypted_config.php
@@ -0,0 +1,13 @@
+<?php 
+
+	// Definimos los parametros //
+	define('METHOD','AES-256-CBC'); 
+	// Metodo recomendado, //
+	define('SECRET_KEY','@%$##!^#^&^&!%@%&$^^$@$^&@!^%#%^*%#&$#^$*$%@*$&&#!$!%#@*&&#$@@&%'); 
+	// Genera tus claves seguras en https://lastpass.com/generatepassword.php //
+	define('SECREY_IV','2447148094677280832782993353546852031310569309554344621539661202');
+	// Genera tus claves seguras en https://lastpass.com/generatepassword.php //
+
+
+
+?>
